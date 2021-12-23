aff7e6ee542c0c46723d831bcb25f9993f8ea1bd
sushanthumane <96003928+sushanthumane@users.noreply.github.com>
2021-12-23 12:51:57 +0530

    Add files via upload

---
+++ src/main/java/com/simplilearn/CICDAWSProject/CicdawsProjectApplication.java
@@ -0,0 +1,13 @@
+package com.simplilearn.CICDAWSProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdawsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdawsProjectApplication.class, args);
+	}
+
+}
