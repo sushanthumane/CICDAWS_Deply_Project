d515df76b268d027db0996db18b4ac50cefcb05e
sushanthumane <96003928+sushanthumane@users.noreply.github.com>
2021-12-23 12:57:50 +0530

    update

---
+++ src/test/java/com/simplilearn/CICDAWSProject/CicdawsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplilearn.CICDAWSProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdawsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
